0278e9359b8c3a2bb099cb3dd91a90a16665e4e4
namgivu <namgivu@gmail.com>
2017-08-08 14:32:45 +0700

    1st run

---
+++ go_tour/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; export SCRIPT_HOME=$s
+
+go run $SCRIPT_HOME/main.go
