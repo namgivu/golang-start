bb8424bc4876a9042ded46fa48ab5312888b6d59
namgivu <namgivu@gmail.com>
2017-09-07 11:35:01 +0700

    review

--- try01_package_multiple_file/01.build-n-run.sh
+++ try01_package_multiple_file/01.build-n-run.sh
@@ -3,16 +3,13 @@
 #get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; export SCRIPT_HOME=$s
 
-curDir=`pwd`
-
 #build output
 NAME='try01'
 
 #build it
 cd $SCRIPT_HOME
 go build -o $NAME
+cd --
 
 #run it
 exec "$SCRIPT_HOME/$NAME"
-
-cd $curDir
