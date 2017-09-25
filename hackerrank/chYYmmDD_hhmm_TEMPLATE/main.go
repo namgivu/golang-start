3ba46deea477eaacab4c357ad4497eabd3184536
namgivu <namgivu@gmail.com>
2017-09-25 16:55:09 +0700

    hackerrank template DONE

--- hackerrank/chYYmmDD_hhmm_TEMPLATE/main.go
+++ hackerrank/chYYmmDD_hhmm_TEMPLATE/main.go
@@ -7,24 +7,31 @@ import (
 	"github.com/namgivu/golang-start/hackerrank/util"
 )
 
-//const INPUT_FILE 	= "input.txt"
-const INPUT_FILE	= "/home/namgvu/gows/src/github.com/namgivu/golang-start/hackerrank/chYYmmDD_hhmm_TEMPLATE/input.txt" //TODO how to use relative path here
-const OUTPUT_FILE	= "/home/namgvu/gows/src/github.com/namgivu/golang-start/hackerrank/chYYmmDD_hhmm_TEMPLATE/output.txt" //TODO how to use relative path here
-
+const CHALLENGE_NAME	= "chYYmmDD_hhmm_TEMPLATE"
 
 func main() {
 
-	/*region redirect stdio*/
-	var REDIRECT_STDIO_2_FILE bool
-	REDIRECT_STDIO_2_FILE = false
-	REDIRECT_STDIO_2_FILE = true //turn this on to redirect stdin/stdout to file ref. TODO https://stackoverflow.com/q/46399395/248616
+	/*region io setting*/
 
-	if REDIRECT_STDIO_2_FILE {
-		inFile, outFile := util.RedirectStdio2File(INPUT_FILE, OUTPUT_FILE)
-		defer inFile.Close()
-		defer outFile.Close()
-	}
-	/*endregion redirect stdio*/
+		var REDIRECT_STDIO_2_FILE bool
+		REDIRECT_STDIO_2_FILE = false
+		REDIRECT_STDIO_2_FILE = true //turn this on to redirect stdin/stdout to file ref. TODO https://stackoverflow.com/q/46399395/248616
+
+		/*region file path config*/
+		var IO_HOME					= util.StrFormat("{HACKERRANK_HOME}/{CHALLENGE_NAME}", "{HACKERRANK_HOME}", util.HACKERRANK_HOME, "{CHALLENGE_NAME}", CHALLENGE_NAME)
+		var INPUT_FILE			= util.StrFormat("{IO_HOME}/input.txt", "{IO_HOME}", IO_HOME)
+		var OUTPUT_FILE			= util.StrFormat("{IO_HOME}/output.txt", "{IO_HOME}", IO_HOME)
+		/*endregion file path config*/
+
+		/*region redirect stdio*/
+		if REDIRECT_STDIO_2_FILE {
+			inFile, outFile := util.RedirectStdio2File(INPUT_FILE, OUTPUT_FILE)
+			defer inFile.Close()
+			defer outFile.Close()
+		}
+		/*endregion redirect stdio*/
+
+	/*endregion io setting*/
 
 	//read N int on line 01
 	var N int
