c4a9777842a47865cc9b74b0968e5d77b983fead
namgivu <namgivu@gmail.com>
2017-09-25 18:13:28 +0700

    more challenges

---
+++ hackerrank/ch170925_1733_triplet-compare/main.go
@@ -0,0 +1,60 @@
+package main
+
+/*
+https://www.hackerrank.com/challenges/compare-the-triplets
+*/
+
+import (
+	"fmt"
+	"github.com/namgivu/golang-start/hackerrank/util"
+)
+
+const CHALLENGE_NAME = "ch170925_1733_triplet-compare"
+
+func main() {
+
+	/*region io setting*/
+
+	var REDIRECT_STDIO_2_FILE bool
+	REDIRECT_STDIO_2_FILE = false
+	REDIRECT_STDIO_2_FILE = true //turn this on to redirect stdin/stdout to file ref. TODO https://stackoverflow.com/q/46399395/248616
+
+	/*region file path config*/
+	var IO_HOME = util.StrFormat("{HACKERRANK_HOME}/{CHALLENGE_NAME}", "{HACKERRANK_HOME}", util.HACKERRANK_HOME, "{CHALLENGE_NAME}", CHALLENGE_NAME)
+	var INPUT_FILE = util.StrFormat("{IO_HOME}/input.txt", "{IO_HOME}", IO_HOME)
+	var OUTPUT_FILE = util.StrFormat("{IO_HOME}/output.txt", "{IO_HOME}", IO_HOME)
+	/*endregion file path config*/
+
+	/*region redirect stdio*/
+	if REDIRECT_STDIO_2_FILE {
+		inFile, outFile := util.RedirectStdio2File(INPUT_FILE, OUTPUT_FILE)
+		defer inFile.Close()
+		defer outFile.Close()
+	}
+	/*endregion redirect stdio*/
+
+	/*endregion io setting*/
+
+	a := make([]int, 3)
+	fmt.Scanf("%d %d %d", &a[0], &a[1], &a[2])
+
+	b := make([]int, 3)
+	fmt.Scanf("%d %d %d", &b[0], &b[1], &b[2])
+
+	var alice,bob int
+	n := len(a)
+	for i:=0; i<n; i++ {
+		checkPoint(a[i], b[i], &alice, &bob)
+	}
+
+	fmt.Println(alice, bob)
+}
+
+func checkPoint(ai, bi int, alice, bob *int) {
+	if ai>bi {
+		*alice++
+	}
+	if ai<bi {
+		*bob++
+	}
+}
---
+++ hackerrank/ch170925_1744_big-int-sum/main.go
@@ -0,0 +1,61 @@
+package main
+
+/*
+https://www.hackerrank.com/challenges/a-very-big-sum
+*/
+
+import (
+	"fmt"
+	"github.com/namgivu/golang-start/hackerrank/util"
+	"math/big"
+)
+
+const CHALLENGE_NAME = "ch170925_1744_big-int-sum"
+
+func main() {
+
+	/*region io setting*/
+
+	var REDIRECT_STDIO_2_FILE bool
+	REDIRECT_STDIO_2_FILE = false
+	REDIRECT_STDIO_2_FILE = true //turn this on to redirect stdin/stdout to file ref. TODO https://stackoverflow.com/q/46399395/248616
+
+	/*region file path config*/
+	var IO_HOME = util.StrFormat("{HACKERRANK_HOME}/{CHALLENGE_NAME}", "{HACKERRANK_HOME}", util.HACKERRANK_HOME, "{CHALLENGE_NAME}", CHALLENGE_NAME)
+	var INPUT_FILE = util.StrFormat("{IO_HOME}/input.txt", "{IO_HOME}", IO_HOME)
+	var OUTPUT_FILE = util.StrFormat("{IO_HOME}/output.txt", "{IO_HOME}", IO_HOME)
+	/*endregion file path config*/
+
+	/*region redirect stdio*/
+	if REDIRECT_STDIO_2_FILE {
+		inFile, outFile := util.RedirectStdio2File(INPUT_FILE, OUTPUT_FILE)
+		defer inFile.Close()
+		defer outFile.Close()
+	}
+	/*endregion redirect stdio*/
+
+	/*endregion io setting*/
+
+	var N int
+	fmt.Scanf("%d", &N)
+
+	a := make([]*big.Int, N)
+	for i:=0; i<N; i++ {
+		var s string
+		fmt.Scanf("%s", &s)
+
+		bi := big.NewInt(0)
+		bi.SetString(s, 10)
+
+		a[i] = bi
+	}
+
+	//fmt.Println(a)
+
+	s := big.NewInt(0)
+	for _,bi := range a {
+		s.Add(s,bi)
+	}
+
+	fmt.Print(s)
+}
