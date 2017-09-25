1d939a04b8a6d1c6a3686e6fdfbf7a20bc3dbaa7
namgivu <namgivu@gmail.com>
2017-09-25 17:06:47 +0700

    hackerrank @ warm up

---
+++ hackerrank/ch170925_1345_warm-up/main.go
@@ -0,0 +1,49 @@
+package main
+
+/*
+https://www.hackerrank.com/challenges/solve-me-first
+*/
+
+import (
+	"github.com/namgivu/golang-start/hackerrank/util"
+	"fmt"
+)
+
+const CHALLENGE_NAME	= "ch170925_1345_warm-up"
+
+
+func solveMeFirst(a uint32,b uint32) uint32{
+	return a+b
+}
+
+
+func main() {
+
+	/*region io setting*/
+
+	var REDIRECT_STDIO_2_FILE bool
+	REDIRECT_STDIO_2_FILE = false
+	REDIRECT_STDIO_2_FILE = true //turn this on to redirect stdin/stdout to file ref. TODO https://stackoverflow.com/q/46399395/248616
+
+	/*region file path config*/
+	var IO_HOME					= util.StrFormat("{HACKERRANK_HOME}/{CHALLENGE_NAME}", "{HACKERRANK_HOME}", util.HACKERRANK_HOME, "{CHALLENGE_NAME}", CHALLENGE_NAME)
+	var INPUT_FILE			= util.StrFormat("{IO_HOME}/input.txt", "{IO_HOME}", IO_HOME)
+	var OUTPUT_FILE			= util.StrFormat("{IO_HOME}/output.txt", "{IO_HOME}", IO_HOME)
+	/*endregion file path config*/
+
+	/*region redirect stdio*/
+	if REDIRECT_STDIO_2_FILE {
+		inFile, outFile := util.RedirectStdio2File(INPUT_FILE, OUTPUT_FILE)
+		defer inFile.Close()
+		defer outFile.Close()
+	}
+	/*endregion redirect stdio*/
+
+	/*endregion io setting*/
+
+	var a, b, res uint32
+	fmt.Scanf("%v\n%v", &a,&b)
+	res = solveMeFirst(a,b)
+	fmt.Println(res)
+
+}
