601d3f96de6868ac973bd586d6a2c18a92c1f8d5
namgivu <namgivu@gmail.com>
2017-09-26 13:47:37 +0700

    more challenge

---
+++ hackerrank/ch170926_1345_birthday-candle/main.go
@@ -0,0 +1,60 @@
+package main
+
+/*
+https://www.hackerrank.com/challenges/birthday-cake-candles
+*/
+
+import (
+	"fmt"
+	"github.com/namgivu/golang-start/hackerrank/util"
+)
+
+const CHALLENGE_NAME = "ch170926_1345_birthday-candle"
+
+func main() {
+
+	/*region io setting*/
+
+	var REDIRECT_STDIO_2_FILE bool
+	REDIRECT_STDIO_2_FILE = false
+	REDIRECT_STDIO_2_FILE = true //turn this on to redirect stdin/stdout to file ref. TODO https://stackoverflow.com/q/46399395/248616
+
+	/*region file path config*/
+	var IO_HOME = util.StrFormat("{HACKERRANK_HOME}/{CHALLENGE_NAME}", "{HACKERRANK_HOME}", util.HACKERRANK_HOME, "{CHALLENGE_NAME}", CHALLENGE_NAME)
+	var INPUT_FILE = util.StrFormat("{IO_HOME}/input.txt", "{IO_HOME}", IO_HOME)
+	var OUTPUT_FILE = util.StrFormat("{IO_HOME}/output.txt", "{IO_HOME}", IO_HOME)
+	/*endregion file path config*/
+
+	/*region redirect stdio*/
+	if REDIRECT_STDIO_2_FILE {
+		inFile, outFile := util.RedirectStdio2File(INPUT_FILE, OUTPUT_FILE)
+		defer inFile.Close()
+		defer outFile.Close()
+	}
+	/*endregion redirect stdio*/
+
+	/*endregion io setting*/
+
+	var N int
+	fmt.Scanf("%d", &N)
+
+	a := make([]int, N)
+	for i:=0; i<N; i++ {
+		fmt.Scanf("%d", &a[i])
+	}
+
+	maxHeight := ^0
+	var count=0
+	for i:=0; i<N; i++ {
+		if a[i] > maxHeight {
+			maxHeight = a[i]
+			count = 0
+		}
+		if a[i] == maxHeight {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+
+}
