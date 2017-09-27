d1b9ce887f29ac23187248ae1399bf74c89ef486
namgivu <namgivu@gmail.com>
2017-09-27 18:14:52 +0700

    lzd dojo - Minh's solution @ verify code

--- lzd-code-dojo/TODO170927_1620_slice-nil-pointer/main.go
+++ lzd-code-dojo/TODO170927_1620_slice-nil-pointer/main.go
@@ -36,9 +36,12 @@ import (
 func removeNilPointers(input interface{}) error {
 	/*author: Minh @minh.trannguyenhao */
 
-	if reflect.ValueOf(input).Kind() == reflect.Ptr {
-		arr := reflect.ValueOf(input).Elem()
-		switch arr.Kind() {
+	inputValue := reflect.ValueOf(input)
+	if inputValue.Kind() == reflect.Ptr { //check if `input` is a pointer; we need it to be pointer so as to remove nil element from it
+		arr := inputValue.Elem() //get value of pointer `inputValue` via .Elem()
+
+		switch arr.Kind() { //proceed with arr to be a Go slice/array only; otherwise, raise error
+
 		case reflect.Slice:
 			fallthrough
 		case reflect.Array:
@@ -46,17 +49,30 @@ func removeNilPointers(input interface{}) error {
 			for i < arr.Len() {
 				indexAtI := arr.Index(i)
 				indexAtIKind := indexAtI.Type().Kind()
-				if (indexAtIKind == reflect.Chan || indexAtIKind == reflect.Func ||
-					indexAtIKind == reflect.Interface || indexAtIKind == reflect.Map ||
-					indexAtIKind == reflect.Ptr || indexAtIKind == reflect.Slice) && indexAtI.IsNil() {
+
+				//TODO Need Minh's explanation for the below check
+				okKind := false ||
+					indexAtIKind == reflect.Chan ||
+					indexAtIKind == reflect.Func ||
+					indexAtIKind == reflect.Interface ||
+					indexAtIKind == reflect.Map ||
+					indexAtIKind == reflect.Ptr ||
+					indexAtIKind == reflect.Slice
+
+				if okKind && indexAtI.IsNil() {//the element at `i` "IS nil"
+
+					//remove element at index `i` via `.AppendSlice()` instead of a normal array/slice removal
 					arr = reflect.AppendSlice(arr.Slice(0, i), arr.Slice(i+1, arr.Len()))
-				} else {
+
+				} else {//the element at `i` is NOT nil
 					i++
 				}
 			}
 			reflect.ValueOf(input).Elem().Set(arr)
+
 		default:
 			return errors.New("input is not pointer of array or slice")
+
 		}
 
 	} else {
