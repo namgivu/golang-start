48d35f8b393c82483e501849753b9e35e4ee97ca
namgivu <namgivu@gmail.com>
2017-11-07 18:29:08 +0700

    sandbox - validate url

---
+++ _vault_/try171107_1730_validate_url/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Printf("%q\n", ValidateHostPort("www.google.com", "80"))
+	fmt.Println("")
+	fmt.Printf("%q\n", ValidateHostPort("www.google.com2222", "80"))
+}
+
+func ValidateHostPort(host string, port string) (isValid bool) {
+	/*
+	 * ref. https://golang.org/pkg/net/
+	 * address = host:port | host | http://domain.com | etc.
+	 */
+	 address := fmt.Sprintf("%s:%s", host, port)
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		isValid = false
+		fmt.Printf("Error:%q\n", err)
+	} else {
+		isValid = true
+		defer conn.Close()
+	}
+	return isValid
+}
