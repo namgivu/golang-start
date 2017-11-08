06fe6b23fc014ac33eafbad8963080150de27aaa
namgivu <namgivu@gmail.com>
2017-11-08 14:15:42 +0700

    sandbox - trim space

---
+++ _vault_/try171108_1400_trim_spaces/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s,t := "",""
+	s = "abb\n" ; t = strings.TrimSpace(s) ; fmt.Printf("%s\n",t)
+	s = "abb\t" ; t = strings.TrimSpace(s) ; fmt.Printf("%s\n",t)
+}
